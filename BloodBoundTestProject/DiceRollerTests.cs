f810e73d9446459fa2f74d73ac26fd79682c030e
Joost <s1072976@student.windesheim.nl>
2023-08-08 15:54:25 +0200

    Added DicePool Function to the DiceRoller

--- BloodBoundTestProject/DiceRollerTests.cs
+++ BloodBoundTestProject/DiceRollerTests.cs
@@ -12,7 +12,38 @@ namespace BloodBoundTestProject
             Diceroller dr = new Diceroller();
             var result = dr.Roll();
             Assert.That(result, Is.GreaterThan(0));
-
         }
+
+        [Test]
+        public void RollPool_TwoInput_ReturnsTwoLenghtArray()
+        {
+            Diceroller dr = new Diceroller();
+            int[] result = dr.RollPool(2);
+            Assert.That(result.Length, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void RollPool_ThreeInput_ReturnsResultGreaterThanZero()
+        {
+            Diceroller dr = new Diceroller();
+            int[] result = dr.RollPool(2);
+            Assert.That(result[1], Is.GreaterThan(0));
+        }
+
+        [Test]
+        public void RollPool_ThreeInput_ReturnsResultlessThanEleven()
+        {
+            Diceroller dr = new Diceroller();
+            int[] result = dr.RollPool(2);
+            Assert.That(result[1], Is.LessThan(11));
+        }
+
+        [Test]
+        public void RollPool_ZeroInput_ThrowsDicePoolException()
+        {
+            Diceroller dr = new Diceroller();
+            Assert.Throws<DicePoolException>(() => dr.RollPool(0));
+        }
+
     }
 }
