c33212867d1ffcb7e35bbcdadf32151b1ab441f6
Joost <s1072976@student.windesheim.nl>
2023-08-08 16:16:09 +0200

    Commented out Unnecesary options to DiceResultRoll container

--- BloodBound/RollResultContainer.cs
+++ BloodBound/RollResultContainer.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace BloodBound
 {
-    public enum DiceState {Crit, BeastlyFail, MessyCrit  }
+    //public enum DiceState {Crit, BeastlyFail, MessyCrit  }
 
     public class RollResultContainer
     {
         public string Description { get; set; }
         public int DicePool { get; set; }
         public int Hunger { get; set; }
-        public DiceState Dicestate { get; set; }
-        public string Result { get; set; }
-        public string OriginalResult { get; set; }
+        //public DiceState Dicestate { get; set; }
+        //public string Result { get; set; }
+        //public string OriginalResult { get; set; }
 
 
     }
