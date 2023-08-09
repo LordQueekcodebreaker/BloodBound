49bb8424242f68c61629b0be431841e0dd690d91
Joost <s1072976@student.windesheim.nl>
2023-08-09 21:18:20 +0200

    Updated rollResultContainer

--- BloodBound/RollResultContainer.cs
+++ BloodBound/RollResultContainer.cs
@@ -6,19 +6,12 @@ using System.Threading.Tasks;
 
 namespace BloodBound
 {
-    //public enum DiceState {Crit, BeastlyFail, MessyCrit  }
-
     public class RollResultContainer
     {
-        public int[]? DiceResult { get; set; }
+        public int[] DiceResult { get; set; }
         public int Successes { get; set; }
         public Boolean Crit { get; set; }
         public Boolean Messy { get; set; }
         public Boolean BeastlyFail { get; set; }
-
-
-
-
-
     }
 }
